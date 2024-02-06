25d7abe0da09c0ee8dfabb4b5f9b774de23dfcf7
Lelzin λ <d3cryptofc@gmail.com>
2024-02-06 01:13:40 -0300

    add `doky/errors:OPEN_AN_ISSUE` variable
    Defining variable for 'Open An Issue' message error.

---
+++ doky/errors.py
@@ -0,0 +1,9 @@
+from . import metadatas
+
+
+# Defining variable for 'Open An Issue' message error.
+OPEN_AN_ISSUE = (
+    'Please open an issue in the project\'s github repository:\n\n'
+    + metadatas.REPOSITORY_ISSUE_URL
+)
+
