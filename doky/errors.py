39049b79a46bac38d8022b03c956f5015f1f1980
Lelzin λ <d3cryptofc@gmail.com>
2024-02-06 01:17:30 -0300

    add `doky/errors:type_error(...)` function
    It creates a TypeError exception containing a custom message error
    for parameters.

--- doky/errors.py
+++ doky/errors.py
@@ -1,3 +1,5 @@
+from typing import Tuple, Any
+
 from . import metadatas
 
 
@@ -7,3 +9,30 @@ OPEN_AN_ISSUE = (
     + metadatas.REPOSITORY_ISSUE_URL
 )
 
+
+def type_error(parameter: Tuple[str, Any], expected_type: type) -> TypeError:
+    """
+    Create a TypeError exception containing a custom message error
+    for parameters.
+
+    Parameters:
+        parameter:
+            A 2-tuple in order: parameter name, parameter value.
+
+        expected_type:
+            The expected type of data.
+
+    Returns:
+        The custom message error exception.
+    """
+    # Unpacking parameter name and parameter value.
+    name, value = parameter
+
+    # Returning the custom message type error object.
+    return TypeError(
+        'Parameter `{}` must be type `{}`, not `{}`'.format(
+            name,
+            expected_type,
+            type(value)
+        )
+    )
