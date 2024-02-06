a02f40e0513a14f189abb48f8cc61fd1d07eb851
Lelzin λ <d3cryptofc@gmail.com>
2024-02-06 12:36:58 -0300

    add `doky/auth_session:AuthSession(session_id: str)` class
    It creates an object to manage the authentication session.

--- doky/auth_session.py
+++ doky/auth_session.py
@@ -1,6 +1,6 @@
 import httpx
 
-from . import metadatas
+from . import metadatas, errors
 
 
 def create_http_client() -> httpx.Client:
@@ -19,4 +19,24 @@ def create_http_client() -> httpx.Client:
             'User-Agent': 'Doky/' + metadatas.VERSION
         }
     )
+
+
+class AuthSession:
+    """
+    Creates an object to manage the authentication session.
+
+    Parameters:
+        session_id:
+            Your session ID from labs.play-with-docker.com
+    """
+    def __init__(self, session_id: str) -> None:
+        # Checking if the `session_id` parameter is a string type.
+        if not isinstance(session_id, str):
+            raise errors.type_error(('session_id', session_id), str)
+
+        # Creating a http client session.
+        self._http_client = create_http_client()
+
+        # Using the same cookie from navigator.
+        self._http_client.cookies['id'] = session_id
 
